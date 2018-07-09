c95e17f9d66cb6391836d095a108b6653959263d
unknown <jlbarbut@ncsu.edu>
2018-07-09 10:13:23 -0400

    styling added

--- clickygame/src/components/Container/Container.js
+++ clickygame/src/components/Container/Container.js
@@ -1,6 +1,10 @@
 import React from "react";
 import "./Container.css";
 
-const Container = props => <div className="container">{props.children}</div>;
+const Container = props => 
+  <div className="container container-fluid text-center">
+    {props.children}
+    <a className="btn" href="/">Start Over</a>
+  </div>;
 
 export default Container;
--- clickygame/src/components/Header/Header.js
+++ clickygame/src/components/Header/Header.js
@@ -2,13 +2,10 @@ import React from "react";
 import "./Header.css";
 
 const Header = props => (
-    <div className="header">
-        <span className="homelink"><a href="/">Start Over</a></span>
-        <span className="title">
-            <h1>{props.directions}</h1>
-            <h5>Click on a flightless bird to earn points, but don't click them more than once!</h5>
-        </span>
-        <span className="score">Score: {props.currScore} | Top Score: {props.topScore}</span>
+    <div className="jumbotron">
+        <h1 className="display-4">{props.directions}</h1>
+        <h3>Click on a flightless bird to earn points, but don't click them more than once!</h3>
+        <p>Score: {props.currScore} | Top Score: {props.topScore}</p>
     </div>
 );
 
